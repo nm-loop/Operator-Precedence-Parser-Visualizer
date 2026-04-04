e1a4de7d14c9ffc8c96e41bb8a4ea84c72116c0a
Niraj kr. maurya <niraj9@zohomail.in>
2026-04-04 20:00:43 +0530

    Refactor parser.h to improve structure and comments

--- parser.h
+++ parser.h
@@ -1,24 +1,22 @@
-// ============================================
-// parser.h - Operator Precedence Parser Header
-// ============================================
-// Project: Operator Precedence Parser Visualizer
-// Authors: Niraj Kumar Maurya (2408390109005)
-//          Meraj Ahamad (2408390109004)
-// Department of Computer Science and Engineering
-// Rajkiya Engineering College, Kannauj
-// ============================================
-
 #ifndef PARSER_H
 #define PARSER_H
 
 #define MAX_STACK 100
 #define MAX_INPUT 100
 
-// Stack structure
+// Stack structure for symbols
 typedef struct {
     char items[MAX_STACK];
     int top;
 } Stack;
+
+// Structure to track both grammar and stack
+typedef struct {
+    char grammarRule[MAX_STACK];
+    char stackContent[MAX_STACK];
+    char remainingInput[MAX_INPUT];
+    char action[MAX_STACK];
+} ParseStep;
 
 // Function prototypes
 void initStack(Stack *s);
@@ -26,13 +24,14 @@ void push(Stack *s, char c);
 char pop(Stack *s);
 char peek(Stack *s);
 int isEmpty(Stack *s);
-int getPrecedence(char op);
 int isOperator(char c);
 int isIdentifier(char c);
-int comparePrecedence(char stackTop, char input);
+int getPrecedence(char op);
 void parseExpression(char *input, int verbose);
 void displayStep(Stack *stack, char *input, int inputPos, char *action);
+void displayDualStep(Stack *stack, char *input, int inputPos, char *grammarRule, char *action);
 int validateExpression(char *expr);
 void printPrecedenceTable(void);
+void printGrammar(void);
 
 #endif
